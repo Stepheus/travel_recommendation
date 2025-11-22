99aff90300eabe6d35cfc3e14ce8ffb76eae1ac7
Stepheus <stephen.iboudah@outlook.com>
2025-11-22 17:39:28 -0600

    moving onto js

---
+++ travel_recommendation.js
@@ -0,0 +1,8 @@
+//Doc element
+
+var reset_button = document.getElementById("reset_button");
+var user_input = document.getElementById("search_bar");
+
+function resetEntry(){
+    user_input.value = "";
+}
